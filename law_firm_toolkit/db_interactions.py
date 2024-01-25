e94d94806093370e58ed5b3818bbe49247cc10dd
alex chauvet <chauvetalex88@gmail.com>
2024-01-25 22:39:58 +0100

    wip

--- law_firm_toolkit/db_interactions.py
+++ law_firm_toolkit/db_interactions.py
@@ -1,12 +1,12 @@
 # Gérer les interactions avec les bases de données.
 
 import sqlite3
-import functions
+import law_firm_toolkit.answer_mails as answer_mails
 
 database = 'data/emails.db'
 
 def insert_answer_mail_html(answer_mail_html_file):
-    row_as_dict = functions.parse_answer_mail_html(answer_mail_html_file)
+    row_as_dict = answer_mails.parse_answer_mail_html(answer_mail_html_file)
     values = ', '.join([f"'{_}'" for _ in row_as_dict.values()])
     sql_insert = f"INSERT INTO tbl_main ({', '.join(row_as_dict.keys())}) VALUES ({values}) ;"
     with sqlite3.connect(database) as cnx:
